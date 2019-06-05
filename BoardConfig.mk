6733fad681437d283daab7ad026d2446a89b38d2
Josh Shafran <j-shafran@ti.com>
2019-06-05 10:07:47 -0500

    beagle_x15: Enable Android Automotive w/ New Lunch Target
    All changes made to baseline enable full Android Automotive in-vehicle
    infotainment (IVI) system, presenting an updated boot animation and
    user interface on AM57xx platforms. Car mode is enabled by default
    and the UI is locked in car mode when using lunch combo
    beagle_x15_auto-userdebug.
    
        https://source.android.com/devices/automotive
        https://developer.android.com/training/cars
    
    Separated the content necessary for Android Auto from baseline
    tablet interface. Result is two lunch combos:
    
    1) beagle_x15-userdebug
    2) beagle_x15_auto-userdebug
    
    Test: Build,Boot with beagle_x15-userdebug and beagle_x15_auto-userdebug
    Test: Look and feel test of Automotive HMI
    Test: Executed test plans CtsCarTestCases and vts-hal-auto
    
    Change-Id: Ia6021d50e803e18ec0241ff190554227a3fda4ae
    Signed-off-by: Josh Shafran <j-shafran@ti.com>
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,13 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/ti/beagle_x15
 BOARD_SEPOLICY_DIRS += \
 	device/ti/beagle_x15/sepolicy
 
+ifeq ($(TARGET_PRODUCT),beagle_x15_auto)
+BOARD_SEPOLICY_DIRS += \
+    packages/services/Car/car_product/sepolicy
+
+DEVICE_MANIFEST_FILE += device/ti/beagle_x15/auto/manifest.xml
+endif
+
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # U-Boot configuration.
